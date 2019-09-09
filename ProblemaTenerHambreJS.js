d5e2814b45215f1905e8ae80c7dd1ac4847bdd2a
AthziriHerrejon <54967484+AthziriHerrejon@users.noreply.github.com>
2019-09-09 18:02:48 -0500

    Add files via upload

---
+++ ProblemaTenerHambreJS.js
@@ -0,0 +1,17 @@
+/*PROBLEMA 1
+
+Pseudocodigo
+console.log ("Tienes Hambre?")
+const Si= "Estas Triste"
+const No= "Estas Feliz"
+var TienesHambre;
+var Si= "Estas Triste"
+var No= "Estas Feliz"*/
+
+
+var TengoHambre= "Si"
+if (TengoHambre==="Si") {
+    console.log ("Estas Triste")
+} else {
+    console.log ("Estas feliz")
+}
