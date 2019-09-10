f4ab1288b1da7e58bc75a1a6eb13d66850f9f995
AthziriHerrejon <54967484+AthziriHerrejon@users.noreply.github.com>
2019-09-09 21:55:39 -0500

    Add files via upload

---
+++ EjercicioNODEJS.js
@@ -0,0 +1,11 @@
+
+    let hambre = process.argv[2];
+    if (hambre =="no") {
+        console.log ("Estoy feliz");
+    } else {
+        if (hambre != "si") {
+    console.log ("no te entiendo");
+        } else {
+            console.log ("estoy triste"); 
+        }   
+    }
